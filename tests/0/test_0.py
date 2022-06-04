b165794b701295fcb89813f4fe4820e545d9b2f7
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-04 15:39:33 +0300

    palvetan

---
+++ tests/0/test_0.py
@@ -0,0 +1,37 @@
+import importlib
+import io
+import sys
+from contextlib import contextmanager
+from unittest import mock
+
+import pytest
+
+MODULE_NAME = '0'
+
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
+
+
+def execute_module(module_name):
+    """Execute module by name."""
+    reload = module_name in sys.modules
+    mod = importlib.import_module(module_name)
+    if reload:
+        importlib.reload(mod)
+    return mod
+
+
+def execute_module_withand_output(capsys, module_name):
+    """Execute module, pass input, return (stdout, stderr)."""
+        execute_module(module_name)
+        x = capsys.readouterr()
+        return x.out, x.err
+
+
+def test_printed(capsys):
+    output = execute_module_with_input_and_output(capsys, MODULE_NAME)[0]
+    assert 'Hello, World!' in output, "Check spelling, the text should be \"Hello, World!\"."
