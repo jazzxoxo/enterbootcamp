e8ceef0ea40ee98671fef50e75edb815a49ddcee
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-09 11:19:50 +0300

    2

---
+++ tests/2/test_2.py
@@ -0,0 +1,48 @@
+import importlib
+import io
+import sys
+from contextlib import contextmanager
+from unittest import mock
+
+import pytest
+
+MODULE_NAME = '4'
+
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
+
+
+def execute_module(module_name):
+    """Execute module by name."""
+    reload = module_name in sys.modules
+    mod = importlib.import_module(module_name)
+    if reload:
+        importlib.reload(mod)
+    return mod
+
+
+def execute_module_with_inputs_and_output(capsys, module_name, input1, input2):
+    """Execute module, pass input, return (stdout, stderr)."""
+    with replace_stdin(io.StringIO(input1)):
+        with replace_stdin(io.StringIO(input2)):
+            execute_module(module_name)
+            x = capsys.readouterr()
+            return x.out, x.err
+
+
+def test_input_asked(monkeypatch):
+    """Whether input() is called."""
+    with mock.patch('builtins.input') as f:
+        execute_module(MODULE_NAME)
+        f.assert_called()
+
+
+def test_printed(capsys):
+    output = execute_module_with_inputs_and_output(capsys, MODULE_NAME, "Tom", "Mike")[0]
+    expected = "Hello Tom and Mike"
+    message = output + " != " + expected
+    assert expected in output, message
