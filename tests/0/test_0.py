f64314c752f3c8fcd737c67a6c4ffcaefc7be49d
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-09 10:27:37 +0300

    test

--- tests/0/test_0.py
+++ tests/0/test_0.py
@@ -1,10 +1,32 @@
 import importlib
+import io
 import sys
+from contextlib import contextmanager
+from unittest import mock
+
 import pytest
 
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
 
-def test_print_hello(capsys):
-    import hello
-    importlib.reload(hello)
-    captured = capsys.readouterr()
-    assert "Hello, World!" in captured.out, "Does the code print \"Hello world!\"? The first letter should be a capital H."
+#def test_print_hello(capsys):
+#    import hello
+#    importlib.reload(hello)
+#    captured = capsys.readouterr()
+#    assert "Hello, World!" in captured.out, "Does the code print \"Hello world!\"? The first letter should be a capital H."
+
+def execute_module(module_name):
+    """Execute module by name."""
+    reload = module_name in sys.modules
+    mod = importlib.import_module(module_name)
+    if reload:
+        importlib.reload(mod)
+    return mod
+
+def test_printed(capsys):
+    output = execute_module(capsys, MODULE_NAME)[0]
+    assert "Hello, World!" in output, "12 is bigger than 10" #pls
