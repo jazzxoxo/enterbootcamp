fab644b1d9f92b8baf837eb37c5fd744de9ffb7a
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-09 10:24:30 +0300

    test

--- tests/0/test_0.py
+++ tests/0/test_0.py
@@ -4,7 +4,7 @@ import pytest
 
 
 def test_print_hello(capsys):
-    import 0
-    importlib.reload(0)
+    import hello
+    importlib.reload(hello)
     captured = capsys.readouterr()
     assert "Hello, World!" in captured.out, "Does the code print \"Hello world!\"? The first letter should be a capital H."
