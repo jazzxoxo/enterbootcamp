cbfeb63f9408fd1735bf32cf7d637096502d4a44
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-16 15:11:50 +0300

    asda

---
+++ tests/17/test_17.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+from "{0}.py".format("17") import lenght
+
+import pytest
+
+@pytest.mark.timeout(1.0)
+def test__17_01():
+    assert length("123") == 3
+    assert length("1") == 1
