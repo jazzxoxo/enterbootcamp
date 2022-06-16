f4425dbd86d992b4bbf313ac93b0d61635ec8b91
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-16 15:15:03 +0300

    asda

--- tests/17/test_17.py
+++ tests/17/test_17.py
@@ -2,11 +2,9 @@ import random
 import string
 import importlib
 
-importlib.import_module("17")
-
 import pytest
 
 @pytest.mark.timeout(1.0)
 def test__17_01():
-    assert 17.length("123") == 3
-    assert 17.length("1") == 1
+    assert importlib.import_module("17").length("123") == 3
+    assert importlib.import_module("17").length("1") == 1
