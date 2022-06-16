a615c14f43c635b3350205de6bb42d0c0a937dd7
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-16 17:28:44 +0300

    21

---
+++ tests/21/test_21.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+import pytest
+
+from read_file import read_file
+
+@pytest.mark.timeout(1.0)
+def test__19_01():
+    assert read_file("hello.txt") == "Hello, World!"
