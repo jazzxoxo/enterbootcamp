02eb148b24d5f4d5c09245b46226102b704f431d
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-16 15:14:45 +0300

    asda

--- tests/17/test_17.py
+++ tests/17/test_17.py
@@ -8,5 +8,5 @@ import pytest
 
 @pytest.mark.timeout(1.0)
 def test__17_01():
-    assert length("123") == 3
-    assert length("1") == 1
+    assert 17.length("123") == 3
+    assert 17.length("1") == 1
