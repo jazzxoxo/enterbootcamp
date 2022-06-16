7c24b83a9f0e421918dfd12f9fb98e71044a027f
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-16 13:13:40 +0300

    asd

---
+++ tests/11/test_11.py
@@ -0,0 +1,39 @@
+import importlib
+import io
+import sys
+from contextlib import contextmanager
+from unittest import mock
+
+import pytest
+
+MODULE_NAME = '10'
+
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
+
+
+def execute_module(module_name):
+    """Execute module by name."""
+    reload = module_name in sys.modules
+    mod = importlib.import_module(module_name)
+    if reload:
+        importlib.reload(mod)
+    return mod
+
+
+def execute_module_with_input_and_output(capsys, module_name, input_string):
+    """Execute module, pass input, return (stdout, stderr)."""
+    with replace_stdin(io.StringIO(input_string)):
+        execute_module(module_name)
+        x = capsys.readouterr()
+        return x.out, x.err
+
+def test_printed(capsys):
+    output = execute_module_with_input_and_output(capsys, MODULE_NAME, "2\n3")[0]
+    expected = "8"
+    message = (output + " != " + expected)
+    assert expected in output, message
