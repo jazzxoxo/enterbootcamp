3e549ef0d5620e2ff456991ab56a5adbb8cca03b
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-17 16:23:55 +0300

    12

--- tests/3/test_3.py
+++ tests/3/test_3.py
@@ -32,14 +32,12 @@ def execute_module_with_input_and_output(capsys, module_name, input_string):
         x = capsys.readouterr()
         return x.out, x.err
 
-
-def test_input_asked(monkeypatch):
-    """Whether input() is called."""
-    with mock.patch('builtins.input') as f:
-        execute_module(MODULE_NAME)
-        f.assert_called()
-
-
-def test_printed(capsys):
+def test_1(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "12")[0]
-    assert 'True' in output, "12 is bigger than 10" #pls
+    expexted = "True"
+    assert expected == output.strip(), "".join((output.strip(), " != ", expected))
+
+def test_1(capsys):
+    output = execute_module_with_input_and_output(capsys, MODULE_NAME, "10")[0]
+    expexted = "False"
+    assert expected == output.strip(), "".join((output.strip(), " != ", expected))
--- tests/4/test_4.py
+++ tests/4/test_4.py
@@ -32,16 +32,7 @@ def execute_module_with_input_and_output(capsys, module_name, input_string):
         x = capsys.readouterr()
         return x.out, x.err
 
-
-def test_input_asked(monkeypatch):
-    """Whether input() is called."""
-    with mock.patch('builtins.input') as f:
-        execute_module(MODULE_NAME)
-        f.assert_called()
-
-
 def test_printed(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "3")[0]
     expected = "0\n1\n2\n3"
-    message = output + " != " + expected
-    assert expected in output, message
+    assert expected == output.strip(), "".join((output.strip(), " != ", expected))
