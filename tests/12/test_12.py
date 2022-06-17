4fc2c197c9fbc98932730b09ee3f7342112c955e
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-17 13:26:39 +0300

    12

--- tests/12/test_12.py
+++ tests/12/test_12.py
@@ -33,15 +33,15 @@ def execute_module_with_input_and_output(capsys, module_name, input_string):
         return x.out, x.err
 
 def errormsg(o, e):
-    m = (o.strip(), " != ", e.strip())
+    m = (str(round(int(o.strip()))), " != ", e.strip())
     return "".join(m)
 
 def test_1(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "3\n4")[0]
     expected = "5"
-    assert output == expected, errormsg(output, expected)
+    assert str(round(int(output.strip()))) == expected, errormsg(output, expected)
 
 def test_2(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "6\n8")[0]
     expected = "10"
-    assert output == expected, errormsg(output, expected)
+    assert str(round(int(output.strip()))) == expected, errormsg(output, expected)
