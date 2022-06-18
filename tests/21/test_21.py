4596687c928e5ac36d00413ea5e4b94ce8779f39
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-18 15:27:00 +0300

    21

--- tests/21/test_21.py
+++ tests/21/test_21.py
@@ -7,4 +7,4 @@ from read_file import read_file
 
 @pytest.mark.timeout(1.0)
 def test__19_01():
-    assert read_file("hello.txt") == "Hello, World!"
+    assert read_file("hello.txt") == "Hello, World!", "".join((read_file("hello.txt"), "!=", "Hello, World!"))
