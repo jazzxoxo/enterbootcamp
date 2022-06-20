1d7e7ca36ada6c03434729a6303eebc019488bd6
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-20 14:00:39 +0300

    asd

--- tests/22/test_22.py
+++ tests/22/test_22.py
@@ -6,6 +6,6 @@ import pytest
 from combine import combine
 
 @pytest.mark.timeout(1.0)
-def test__19_01():
-    assert (combine([1],[0])).sort() == [1,0]
-    assert (combine([1, 2],[3, 4])).sort() == [1,2,3,4]
+def test__22_01():
+    assert combine([1],[0]).sort() == [0,1]
+    assert combine([1, 2],[3, 4]).sort() == [1,2,3,4]
