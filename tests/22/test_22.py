3f575b0ac96386796543eb200ed0be7cd9f95830
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-26 15:00:36 +0300

    sdgasgfs\f\s

--- tests/22/test_22.py
+++ tests/22/test_22.py
@@ -7,5 +7,5 @@ from combine import combine
 
 @pytest.mark.timeout(1.0)
 def test__22_01():
-    assert (combine([1],[0])).sort() == [1,0]
-    assert (combine([1, 2],[3, 4])).sort() == [1,2,3,4]
+    assert combine([1],[0]).sort() == [1,0]
+    assert combine([1, 2],[3, 4]).sort() == [1,2,3,4]
