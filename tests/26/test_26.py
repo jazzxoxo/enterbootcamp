d4664ecedcb789168780577e862c6fc75f41c80c
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-26 15:06:33 +0300

    sdgasgfs\f\s

--- tests/26/test_26.py
+++ tests/26/test_26.py
@@ -7,7 +7,7 @@ from value_from_key import value_from_key
 
 @pytest.mark.timeout(1.0)
 def test__26_01():
-    assert value_from_key({"a":1}, a) == 1
-    assert value_from_key({"a":1, "b":2, "c":3}, c) == 3
-    assert value_from_key({"a":1, "b":2, "c":3}, a) == 1
-    assert value_from_key({"a":1, "b":2, "c":3}, b) == 2
+    assert value_from_key({"a":1}, "a") == 1
+    assert value_from_key({"a":1, "b":2, "c":3}, "c") == 3
+    assert value_from_key({"a":1, "b":2, "c":3}, "a") == 1
+    assert value_from_key({"a":1, "b":2, "c":3}, "b") == 2
