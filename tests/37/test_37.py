8a8396bbe1b3e75d1e83159efa19e32635e7e51c
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-27 16:02:27 +0300

    sdgasgfs\f\s

--- tests/37/test_37.py
+++ tests/37/test_37.py
@@ -3,10 +3,10 @@ import string
 
 import pytest
 
-from palindrome import palindrome
+from tic_tac_toe import tic_tac_toe
 
 @pytest.mark.timeout(1.0)
-def test__35_01():
-    assert palindrome(["X","O","O","","X","O","","",""]) == True
-    assert palindrome(["X","O","","X","O","X","O","",""]) == False
-    assert palindrome(["","","","","","","","",""]) == False
+def test__37_01():
+    assert tic_tac_toe(["X","O","O","","X","O","","",""]) == True
+    assert tic_tac_toe(["X","O","","X","O","X","O","",""]) == False
+    assert tic_tac_toe(["","","","","","","","",""]) == False
