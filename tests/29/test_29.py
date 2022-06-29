75d4567f11bdf66ff7e78240f191461d5e8fa349
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-29 06:47:36 +0300

    asd

--- tests/29/test_29.py
+++ tests/29/test_29.py
@@ -7,4 +7,4 @@ from file_to_dict import file_to_dict
 
 @pytest.mark.timeout(1.0)
 def test__29_01():
-    assert file_to_dict("test.txt") == {1: "Hello", "Hello": "2"}
+    assert file_to_dict("test.txt") == {"1": "Hello", "Hello": "2"}
