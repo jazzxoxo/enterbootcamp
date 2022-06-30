ef72206272c00d3c5120c6f0a3482e85a43eeb72
Mihkel Jesse <mihkel.jesse@gmail.com>
2022-06-30 09:54:52 +0300

    asd

--- tests/36/test_36.py
+++ tests/36/test_36.py
@@ -35,14 +35,14 @@ def execute_module_with_input_and_output(capsys, module_name, input_string):
 def test_1(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "Hello!")[0]
     expected = "Hallo!"
-    assert expected == output.strip(), "".join((repr(output.strip()), " != ", repr(expected)))
+    assert output.strip() == expected
 
 def test_2(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "aAeE")[0]
     expected = "eEaA"
-    assert expected == output.strip(), "".join((repr(output.strip()), " != ", repr(expected)))
+    assert output.strip() == expected
 
 def test_3(capsys):
     output = execute_module_with_input_and_output(capsys, MODULE_NAME, "Hello, Orissaare!")[0]
     expected = "Hallo, Orisseera!"
-    assert expected == output.strip(), "".join((repr(output.strip()), " != ", repr(expected)))
+    assert output.strip() == expected
